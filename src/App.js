1a01f69506af715ec5834f7217831dd147ffb99f
Kirill Mager <kiruha24@mail.ru>
2020-02-19 15:57:27 +0300

    feat: add logic to load and display info about specific Pokemon
    Component for displaying a Pokemon infotmations was moved out to a component folder (component/card).
    
    Function for getting pokemon ( getPokemon() ) was moved out to a independent module (services/pokemon).

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { getAllPokemon } from './services/pokemon';
+import { getAllPokemon, getPokemon } from './services/pokemon';
+import Card from './components/Card/Card'
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,18 +14,39 @@ function App() {
   useEffect(() => {
     async function fetchData() {
       let response = await getAllPokemon(initialUrl);
-      console.log(response);
       setNextUrl(response.next);
       setPrevUrl(response.previous);
+      let pokemon = await loadingPokemon(response.results);
+      console.log(pokemon);
       setLoading(false);
     }
     fetchData();
   }, [])
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord;
+    })
+    );
+
+    setPokemonData(_pokemonData);
+  };
+
   return (
-    <div >
-      { loading ? <h1>Loading...</h1> : (
-        <h1>Data is fetched</h1>
-      ) }
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+        ) : (
+          <>
+            <div className='grid-container'>
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+          </>
+        )}
     </div>
   );
 }
--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -7,3 +7,13 @@ export async function getAllPokemon(url) {
       })
   })
 }
+
+export async function getPokemon(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        resolve(data);
+      })
+  })
+}

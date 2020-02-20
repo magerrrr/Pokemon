7843a67c7f1cccd1a92609f5b7721f191f3387dd
Kirill Mager <kiruha24@mail.ru>
2020-02-20 14:59:10 +0300

    refactor: add ButtonWithTitle component

---
+++ src/components/ButtonWithTitle/ButtonWithTitle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+function ButtonWithTitle({ title, buttonText, clickHandler}) {
+  return(
+    <>
+      <h3 className='header-text'>{title}</h3>
+      <div className='button-container'>
+  <button onClick={clickHandler}>{buttonText}</button>
+      </div>
+    </>
+  )
+}
+
+export default ButtonWithTitle;

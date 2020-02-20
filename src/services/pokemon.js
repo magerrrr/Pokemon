c4da89c91778e1043e4ec4e9c4034d111f000090
Kirill Mager <kiruha24@mail.ru>
2020-02-20 15:02:51 +0300

    refactor: combine 2 same functions to 1

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,14 +1,4 @@
-export async function getAllPokemon(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        resolve(data);
-      })
-  })
-}
-
-export async function getPokemon(url) {
+export async function fetchItemsFromServer(url) {
   return new Promise((resolve, reject) => {
     fetch(url)
       .then(res => res.json())

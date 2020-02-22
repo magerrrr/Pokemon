83a6e8ac1a225bae875cecbdc1d3c3e3dd044f8e
Kirill Mager <kiruha24@mail.ru>
2020-02-22 11:41:43 +0300

    refactor: update Card component

--- src/components/CardsContainer/Card/Card.js
+++ src/components/CardsContainer/Card/Card.js
@@ -24,20 +24,53 @@ function Card({ item }) {
         })}
       </div>
       <div className='Card__info'>
-        <div className='Card__info-two'>
-          <div className='Card__data Card__data--weight'>
+      <h3 className='Card__baseStatsTitle'>Pokemon data:</h3>
+        <div className='Card__info-PokemonData'>
+          <div className='Card__data Card__data--ability'>
             <p className='title'>Weight:</p>
-            <p>{item.weight}</p>
+            <p className='baseStat'>{item.weight}</p>
           </div>
-          <div className='Card__data Card__data--height'>
+          <div className='Card__data Card__data--ability'>
             <p className='title'>Height:</p>
-            <p>{item.height}</p>
+            <p className='baseStat'>{item.height}</p>
           </div>
-        </div>  
-        <div className='Card__data Card__data--ability'>
-          <p className='title'>Ability:</p>
-          <p>{item.abilities[0].ability.name}</p>
+          <div className='Card__data Card__data--ability'>
+            <p className='title'>Ability:</p>
+            <p className='baseStat'>{item.abilities[0].ability.name}</p>
+          </div>
+          <div className='Card__data Card__data--ability'>
+            <p className='title'>Species:</p>
+            <p className='baseStat'>{item.species.name}</p>
+          </div>
         </div>
+        <h3 className='Card__baseStatsTitle'>Base stats:</h3>
+        <div className='Card__baseStats'>
+          <div className='Card__data--baseStats'>
+            <p className='title'>HP:</p>
+            <p className='baseStat'>{item.stats[5].base_stat}</p>
+          </div>
+          <div className='Card__data--baseStats'>
+            <p className='title'>Attack:</p>
+            <p className='baseStat'>{item.stats[4].base_stat}</p>
+          </div>
+          <div className='Card__data--baseStats'>
+            <p className='title'>Defense:</p>
+            <p className='baseStat'>{item.stats[3].base_stat}</p>
+          </div>
+          <div className='Card__data--baseStats'>
+            <p className='title'>Sp. Atc:</p>
+            <p className='baseStat'>{item.stats[2].base_stat}</p>
+          </div>
+          <div className='Card__data--baseStats'>
+            <p className='title'>Sp. Def:</p>
+            <p className='baseStat'>{item.stats[1].base_stat}</p>
+          </div>
+          <div className='Card__data--baseStats'>
+            <p className='title'>Speed:</p>
+            <p className='baseStat'>{item.stats[0].base_stat}</p>
+          </div>
+        </div>
+        <PokemonInfo pokemon={item}/>
       </div>
     </div>
   );

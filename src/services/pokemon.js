8a58d03611e1f6dc1849513b4f36fc74e9356118
Kirill Mager <kiruha24@mail.ru>
2020-02-24 16:02:22 +0300

    refactor: all arrow functions in Pokemon.js

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,7 +1,7 @@
 import { setLoading, setNextUrl, setPrevUrl, setItems, setEvolutionChain } from '../actions';
 import { POKEMON_SPECIES } from '../helpers/urls';
 
-export const getPokemons = (pokemons) => {
+export const getPokemons = pokemons => {
   let _pokemonData = Promise.all(pokemons.map(async pokemonItem => {
     const pokemonRecord = await fetchItemsFromServer(pokemonItem.url);
     return pokemonRecord;
@@ -9,7 +9,7 @@ export const getPokemons = (pokemons) => {
   return _pokemonData;
 }
 
-export const getPokemonsByType = (data) => {
+export const getPokemonsByType = data => {
   const _pokemonData = Promise.all(data.map(async element => {
     const pokemonRecord = await fetchItemsFromServer(element.pokemon.url);
     return pokemonRecord;
@@ -23,77 +23,62 @@ export async function fetchItemsFromServer(url) {
   return data;
 }
 
-export const showAllItems = (url) => {
-  return (dispatch) => {
-    dispatch(setLoading(true));
-    fetchItemsFromServer(url)
-      .then(data => {
-        dispatch(setNextUrl(data.next));
-        dispatch(setPrevUrl(data.previous));
-        return getPokemons(data.results)
-      })
-      .then(data => {
-        dispatch(setItems(data))
-      })
-      .then(() => dispatch(setLoading(false)))
-  }
+export const showAllItems = url => dispatch => {
+  dispatch(setLoading(true));
+  fetchItemsFromServer(url)
+  .then(data => {
+    dispatch(setNextUrl(data.next));
+    dispatch(setPrevUrl(data.previous));
+    return getPokemons(data.results)
+  })
+  .then(data => dispatch(setItems(data)))
+  .then(() => dispatch(setLoading(false)))
 }
 
-export const filterItemsByType = (url) => {
-  return (dispatch) => {
-    dispatch(setLoading(true));
-    fetchItemsFromServer(url)
-      .then(data => {
-        return getPokemonsByType(data.pokemon)
-      })
-      .then(data => {
-        return dispatch(setItems(data))
-      })
-      .then(() => dispatch(setLoading(false)))
-  }
+export const filterItemsByType = url => dispatch => {
+  dispatch(setLoading(true));
+  fetchItemsFromServer(url)
+  .then(data => getPokemonsByType(data.pokemon))
+  .then(data => dispatch(setItems(data)))
+  .then(() => dispatch(setLoading(false)))
 }
 
-export const getEvolutionChain = (id) => {
-  return (dispatch) => {
-    fetchItemsFromServer(POKEMON_SPECIES + id)
-    .then(data => {
-      return fetchItemsFromServer(data.evolution_chain.url)
-    })
-    .then(data => {
-      const evolutionChain = [
-        data.chain.species,
-        data.chain.evolves_to[0].species,
-        data.chain.evolves_to[0].evolves_to[0].species
-      ]
-      return Promise.all([
-        fetchItemsFromServer(evolutionChain[0].url),
-        fetchItemsFromServer(evolutionChain[1].url),
-        fetchItemsFromServer(evolutionChain[2].url)
-      ])
-    })
-    .then((data) => dispatch(setEvolutionChain(data)))
-  }
+export const getEvolutionChain = id => dispatch => {
+  fetchItemsFromServer(POKEMON_SPECIES + id)
+  .then(data => fetchItemsFromServer(data.evolution_chain.url))
+  .then(data => {
+    const evolutionChain = [
+      data.chain.species,
+      data.chain.evolves_to[0].species,
+      data.chain.evolves_to[0].evolves_to[0].species
+    ]
+    return Promise.all([
+      fetchItemsFromServer(evolutionChain[0].url),
+      fetchItemsFromServer(evolutionChain[1].url),
+      fetchItemsFromServer(evolutionChain[2].url)
+    ])
+  })
+  .then(data => dispatch(setEvolutionChain(data)))
 }
 
-export const getItemsPart = (url) => {
+export const getItemsPart = url => {
   if (!url) return;
-  return (dispatch) => {
+  return dispatch => {
     dispatch(setLoading(false));
     fetchItemsFromServer(url)
-      .then(data => {
-        dispatch(setNextUrl(data.next));
-        dispatch(setPrevUrl(data.previous));
-        return getPokemons(data.results);
-      })
-      .then((data) => dispatch(setItems(data)))
-      .then(() => dispatch(setLoading(false)))
+    .then(data => {
+      dispatch(setNextUrl(data.next));
+      dispatch(setPrevUrl(data.previous));
+      return getPokemons(data.results);
+    })
+    .then(data => dispatch(setItems(data)))
+    .then(() => dispatch(setLoading(false)))
   }
 }
 
-export const getMoves = (pokemon) => {
-  return pokemon.moves.map(move => move.move.name
-    .toLowerCase()
-    .split('-')
-    .map(subStr => `${subStr[0].toUpperCase()}${subStr.slice(1)}`)
-    .join(' '));
-}
+export const getMoves = pokemon => pokemon.moves.map(move => move.move.name
+  .toLowerCase()
+  .split('-')
+  .map(subStr => `${subStr[0].toUpperCase()}${subStr.slice(1)}`)
+  .join(' ')
+);
